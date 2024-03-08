6d8b9ab0dd518e2ad3ae64954f0bdd70a6be1283
nakor <nakor22@student.sdu.dk>
2024-03-08 11:17:39 +0100

    Did stuff

--- src/opgave02/Opgave02.java
+++ src/opgave02/Opgave02.java
@@ -2,11 +2,14 @@ package opgave02;
 
 import opgave02.models.Range;
 
+import java.util.Iterator;
+
 public class Opgave02 {
     public static void main(String[] args) {
         Range range = new Range(5, 10);
-        for (Integer integer : range) {
-            System.out.println(integer);
+        Iterator<Integer> iterator = range.iterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next());
         }
     }
 }
